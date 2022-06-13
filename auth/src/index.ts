2f831137a0ed6e9d6cb17193449057d9b0aebcb2
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-06-13 12:46:44 +0530

    tests for signin and signout added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,5 @@
-import express from "express";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import "express-async-errors";
-
-import { errorHandler } from "./middlewares/error-handler";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-
-app.set("trust proxy", true);
-
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.use(errorHandler);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
