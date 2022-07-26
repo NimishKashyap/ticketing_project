5021809cb67c87b75dd7a9e727eb8c9ddd50b615
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-07-26 09:07:54 +0530

    feat: Gracefull termination of channels

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -9,9 +9,23 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
+  stan.on("close", () => {
+    console.log("NATS is closing");
+    process.exit();
+  });
+
+  // sets manual ackowledgement mode to true
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  // to set options, chain the methods
+
+  // default behavior is: auto-mark done after recieving it
+  // if service is failed, the event will be lost
+  // manually acknowledge
+
   const subscription = stan.subscribe(
     "ticket:created",
-    "orders-service-queue-group"
+    "orders-service-queue-group",
+    options
   );
 
   subscription.on("message", (message: Message) => {
@@ -19,5 +33,11 @@ stan.on("connect", () => {
     if (typeof data === "string") {
       console.log(`Received event#${message.getSequence()}, with data:${data}`);
     }
+
+    message.ack();
   });
 });
+
+// Change the process if used in windows
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
