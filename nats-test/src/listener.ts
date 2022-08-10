32b58563472656aa25e14b28740926f2f61f0c0f
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-08-10 22:40:43 +0530

    added listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 console.clear();
 
@@ -15,7 +15,13 @@ stan.on("connect", () => {
   });
 
   // sets manual ackowledgement mode to true
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  // setDeliverAllAvailable() is used to deliver all messages in the queue
+  // setDurableName() is used to set the name of the durable queue
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("order-service");
   // to set options, chain the methods
 
   // default behavior is: auto-mark done after recieving it
@@ -41,3 +47,48 @@ stan.on("connect", () => {
 // Change the process if used in windows
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, message: Message): void;
+
+  private client: Stan;
+
+  protected ackWaitTime = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setDurableName(this.queueGroupName);
+  }
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (message: Message) => {
+      console.log(`
+        Message Received: ${this.subject} / ${this.queueGroupName} 
+      `);
+
+      const parsedData = this.parseMessage(message);
+      this.onMessage(parsedData, message);
+    });
+  }
+
+  parseMessage(message: Message): string {
+    const data = message.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf-8"));
+  }
+}
