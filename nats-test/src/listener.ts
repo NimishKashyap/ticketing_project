80ce09c5720093de54a9e5226082f3fcc386d0ea
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-08-13 10:56:45 +0530

    Feat: Added event types

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,7 @@
 import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
+
 console.clear();
 
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -17,78 +19,16 @@ stan.on("connect", () => {
   // sets manual ackowledgement mode to true
   // setDeliverAllAvailable() is used to deliver all messages in the queue
   // setDurableName() is used to set the name of the durable queue
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("order-service");
+
   // to set options, chain the methods
 
   // default behavior is: auto-mark done after recieving it
   // if service is failed, the event will be lost
   // manually acknowledge
 
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
-
-  subscription.on("message", (message: Message) => {
-    const data = message.getData();
-    if (typeof data === "string") {
-      console.log(`Received event#${message.getSequence()}, with data:${data}`);
-    }
-
-    message.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 // Change the process if used in windows
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, message: Message): void;
-
-  private client: Stan;
-
-  protected ackWaitTime = 5 * 1000;
-
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setDurableName(this.queueGroupName);
-  }
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on("message", (message: Message) => {
-      console.log(`
-        Message Received: ${this.subject} / ${this.queueGroupName} 
-      `);
-
-      const parsedData = this.parseMessage(message);
-      this.onMessage(parsedData, message);
-    });
-  }
-
-  parseMessage(message: Message): string {
-    const data = message.getData();
-    return typeof data === "string"
-      ? JSON.parse(data)
-      : JSON.parse(data.toString("utf-8"));
-  }
-}
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -6,19 +7,29 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "Concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
   // we can share only strings in NATS
   // we have to convert to JSON
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "concert",
+  //   price: 20,
+  // });
 
-  // this data is called a message
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published");
-  });
+  // // this data is called a message
+  // stan.publish("ticket:created", data, () => {
+  //   console.log("Event published");
+  // });
 });
