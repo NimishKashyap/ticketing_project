5b87ed3e0174274b9ad159dc584318a66d7a22ae
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-08-14 18:18:03 +0530

    Skaffold: Orders Service

---
+++ orders/src/app.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import cookieSession from "cookie-session";
+import "express-async-errors";
+
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@nimishkashyap031/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
