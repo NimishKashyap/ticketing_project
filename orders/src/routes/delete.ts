b0f79fa5dc377c04411f5aa755f6f06fb1a2ebdc
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-10-27 17:52:31 +0530

    Feat: added tests for concurrency inside order service

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -29,6 +29,7 @@ router.delete(
     // publishes an event saying order is cancelled
     new OrderCancelledPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.version,
       ticket: {
         id: order.ticket.id,
       },
