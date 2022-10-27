058a420f21d4564d4ad80d5f2fec206e15295284
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-10-27 18:50:58 +0530

    feat: finalised test for order-created-listener ts

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,25 @@ import {
 } from "@nimishkashyap031/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/tickets";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+    // if no ticket throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    // mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+    // save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
+  }
 }
